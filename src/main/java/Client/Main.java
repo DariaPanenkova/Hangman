175a277b640b126d137c1ff816f774445fe0e056
Panenkova Daria <dariapanenkova@gmail.com>
2025-10-22 20:41:53 +0300

    add game loop

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -3,8 +3,7 @@ package Client;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -16,7 +15,15 @@ import java.util.stream.Stream;
  * У игрока 6 попыток на ввод буквы (голова, туловище, 2 руки и 2 ноги)*/
 public class Main {
     private static final int MIN_LENGTH = 3;
+    private static final int MAX_TRIES = 6;
     private static final Path dictionaryPath = Path.of("src", "main","resources","singular.txt");
+
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static final String GAME_STATE_WON = "Слово угадано!";
+    private static final String GAME_STATE_OVER = "Вы проиграли!";
+    private static final String GAME_STATE_GAME_NOT_OVER = "Игра не закончена";
 
     public static void main(String[] args) {
         //получаем словарь слов не менее 5 букв
@@ -25,9 +32,8 @@ public class Main {
         //или пока есть не отгаданные буквы в слове
         //если буква есть в слове - устанавливаем ее на правильное(ые) место(а)
         //если буквы нет - увеличиваем счетчик ошибок (максимум - 6) и рисуем новое состояние виселицы
-        List<String> dictionary = getDictionary(dictionaryPath);
 
-        System.out.println((long) dictionary.size());
+        startGame();
     }
 
     public static List<String> getDictionary(Path dictionaryPath){
@@ -42,5 +48,109 @@ public class Main {
         }
 
         return dictionary;
+    }
+
+    public static String getRandomWord(List<String> dictionary){
+        int index = random.nextInt(dictionary.size());
+        return dictionary.get(index);
+    }
+
+    public static String getCurrentWordState(String searchWord, Set<Character> rightCharsSet) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : searchWord.toCharArray()) {
+            if (rightCharsSet.contains(c)) {
+                result.append(c); // Оставляем символ как есть
+            } else if (Character.isLetter(c)) {
+                result.append('_'); // Заменяем букву на _
+            }
+        }
+        return result.toString();
+    }
+
+    public static char inputChar(Set<Character> correctCharsSet, Set<Character> incorrectCharsSet){
+        System.out.println("Введите букву для проверки: ");
+
+        do {
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.isEmpty()) {
+                System.out.println("Пустой ввод. Введите букву: ");
+                continue;
+            }
+
+            if (input.length() > 1) {
+                System.out.println("Введено больше одного символа.Введите букву: ");
+                continue;
+            }
+
+            char character = input.charAt(0);
+
+            if (!input.matches("[а-яё]")) {
+                System.out.println("Введите русскую букву: ");
+                continue;
+            }
+
+            if (correctCharsSet.contains(character)
+                || incorrectCharsSet.contains(character)){
+                System.out.println("Вы уже проверяли букву " + input +".Введите русскую букву: ");
+                continue;
+            }
+
+            return character;
+        } while (true);
+    }
+
+    public static void startGame(){
+        List<String> dictionary = getDictionary(dictionaryPath);
+        while(true){
+            System.out.println("Хотите начать игру? 1 - да 0 - нет");
+            String input = scanner.nextLine();
+
+            if (Objects.equals(input, "1")){
+                String searchWord = getRandomWord(dictionary);
+                System.out.println(searchWord);
+                startGameLoop(searchWord);
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void startGameLoop(String searchWord){
+        Set<Character> correctCharSet = new HashSet<>();
+        Set<Character> incorrectCharSet = new HashSet<>();
+
+        do{
+            char charTry = inputChar(correctCharSet, incorrectCharSet);
+
+            if (searchWord.contains(String.valueOf(charTry))){
+                correctCharSet.add(charTry);
+            }else{
+                incorrectCharSet.add(charTry);
+            }
+
+            String currentWordState = getCurrentWordState(searchWord, correctCharSet);
+            System.out.println(currentWordState);
+            String gameState = checkGameState(searchWord, currentWordState, incorrectCharSet.size());
+            System.out.println("Ошибки (" + incorrectCharSet.size() + "):" + incorrectCharSet);
+            if (!Objects.equals(gameState, GAME_STATE_GAME_NOT_OVER)){
+                System.out.println(gameState);
+                System.out.println("Загаданное слово: " + searchWord);
+                return;
+            }
+        }while(true);
+    }
+
+    public static String checkGameState(String searchWord, String currentWordState, int incorrectTries){
+        if (Objects.equals(searchWord, currentWordState)){
+            return GAME_STATE_WON;
+        } else if (currentWordState.contains("_")) {
+            if (incorrectTries >= MAX_TRIES) {
+                return GAME_STATE_OVER;
+            }
+        }
+
+        return GAME_STATE_GAME_NOT_OVER;
     }
 }
