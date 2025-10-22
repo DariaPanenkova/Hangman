91c2615b7f2889f65a20212c45eeb92e698dbd81
Panenkova Daria <dariapanenkova@gmail.com>
2025-10-22 18:40:43 +0300

    add getDictionary function

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,17 +1,45 @@
 package Client;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+/**
+ * Бот загадывает слово - существительное в именительном падеже
+ * Загаданное слово содержит более 5 букв
+ * Изначально все буквы слова неизвестны для игрока
+ * Игрок вводит буквы по-одной, регистр не важен
+ * У игрока 6 попыток на ввод буквы (голова, туловище, 2 руки и 2 ноги)*/
+public class Main {
+    private static final int MIN_LENGTH = 3;
+    private static final Path dictionaryPath = Path.of("resources","singular.txt");
+
+    public static void main(String[] args) {
+        //получаем словарь слов не менее 5 букв
+        //выбираем рандомно слово из словаря
+        //запрашиваем букву у пользователя, пока игрок не проиграл(попытки закончились)
+        //или пока есть не отгаданные буквы в слове
+        //если буква есть в слове - устанавливаем ее на правильное(ые) место(а)
+        //если буквы нет - увеличиваем счетчик ошибок (максимум - 6) и рисуем новое состояние виселицы
+        List<String> dictionary = getDictionary(dictionaryPath);
+
+    }
+
+    public static List<String> getDictionary(Path dictionaryPath){
+        List<String> dictionary = new ArrayList<>();
+
+        try (Stream<String> lines = Files.lines(dictionaryPath)) {
+            dictionary = lines.filter(line -> line.length() > MIN_LENGTH)
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
         }
+
+        return dictionary;
     }
 }
